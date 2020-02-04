dd6c0cf7aed618ece4a1dcd784b3012177251537
Brahim Bahaida <bbahieda@gmail.com>
2020-02-04 14:29:34 +0000

    setup parcel and add react from npm pkg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-// code
-const Pet = ({ name, type, breed }) => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, type),
-    React.createElement('h2', {}, breed)
-  );
-};
-
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
 const App = () => {
   return React.createElement(
     'div',
@@ -20,4 +12,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+render(React.createElement(App), document.getElementById('root'));
