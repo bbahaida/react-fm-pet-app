cdb6fb4c0711b9f823e0d96540de62cbf04a17a3
Brahim Bahaida <bbahieda@gmail.com>
2020-02-05 20:33:14 +0000

    using the react context to manage a cross components state

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from 'react';
 import pet from '@frontendmasters/pet';
 import Carousel from './Carousel';
 import ErrorBoundary from './ErrorBoundary';
+import ThemeContext from './ThemeContext';
 
 class Details extends React.Component {
   state = {
@@ -36,7 +37,11 @@ class Details extends React.Component {
           <h2>
             {animal} - {breed} - {location}
           </h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
